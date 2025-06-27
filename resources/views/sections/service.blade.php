aa2b46ab0fb0d242efd8ee85435fa96b59f5283a
abhibadadhe <abhibadadhe33@gmail.com>
2025-06-27 22:51:56 +0530

    Initial commit

---
+++ resources/views/sections/service.blade.php
@@ -0,0 +1,82 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title text-center text-primary text-uppercase">Our Services</h6>
+            <h1 class="mb-5">Explore Our <span class="text-primary text-uppercase">Services</span></h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-hotel fa-2x text-primary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">Rooms & Appartment</h5>
+                    <p class="text-body mb-0">We are committed to delivering quality and excellence, combining innovation with integrity. Our team believes in transparency, consistency, and putting our clients first—every step of the way.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-utensils fa-2x text-primary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">Food & Restaurant</h5>
+                    <p class="text-body mb-0">Our services are designed to deliver value, precision, and results. We combine innovation with expertise to provide reliable solutions that meet your unique needs—efficiently and professionally.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-spa fa-2x text-primary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">Spa & Fitness</h5>
+                    <p class="text-body mb-0">We deliver professional, reliable, and tailored solutions designed to meet your unique needs with precision and care.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-swimmer fa-2x text-primary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">Sports & Gaming</h5>
+                    <p class="text-body mb-0">We provide dependable and client-focused services, combining expertise with a commitment to quality and consistency.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-glass-cheers fa-2x text-primary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">Event & Party</h5>
+                    <p class="text-body mb-0">We offer reliable, efficient, and client-focused services, built on a foundation of trust, quality, and consistency.</p>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+                <a class="service-item rounded" href="">
+                    <div class="service-icon bg-transparent border rounded p-1">
+                        <div
+                            class="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
+                            <i class="fa fa-dumbbell fa-2x text-priWe are dedicated to delivering reliable, high-quality services with a focus on integrity, precision, and customer satisfactionmary"></i>
+                        </div>
+                    </div>
+                    <h5 class="mb-3">GYM & Yoga</h5>
+                    <p class="text-body mb-0">We are dedicated to delivering reliable, high-quality services with a focus on integrity, precision, and customer satisfaction.</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/sections/testimonial.blade.php
@@ -0,0 +1,42 @@
+<div class="container-xxl testimonial my-5 py-5 bg-dark wow zoomIn" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="owl-carousel testimonial-carousel py-5">
+            <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                <p>Their dedication and attention to detail truly stood out—every step of the process was handled with care, professionalism, and a commitment to excellence</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-1.jpg"
+                         style="width: 45px; height: 45px;">
+                    <div class="ps-3">
+                        <h6 class="fw-bold mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+                <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+            </div>
+            <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                <p>Working with them was a seamless experience—consistent, professional, and truly focused on delivering quality at every stage</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-2.jpg"
+                         style="width: 45px; height: 45px;">
+                    <div class="ps-3">
+                        <h6 class="fw-bold mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+                <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+            </div>
+            <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                <p>Their service was consistently reliable and professional, exceeding expectations with every detail handled smoothly and efficiently</p>
+                <div class="d-flex align-items-center">
+                    <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-3.jpg"
+                         style="width: 45px; height: 45px;">
+                    <div class="ps-3">
+                        <h6 class="fw-bold mb-1">Client Name</h6>
+                        <small>Profession</small>
+                    </div>
+                </div>
+                <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+            </div>
+        </div>
+    </div>
+</div>
